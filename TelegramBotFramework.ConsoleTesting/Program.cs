ea26e4a12f1cf8259feeff4546079c4b30e0644c
msg_kurt <artemkuryanov@gmail.com>
2019-04-08 15:41:04 +0300

    bot key removed

--- TelegramBotFramework.ConsoleTesting/Program.cs
+++ TelegramBotFramework.ConsoleTesting/Program.cs
@@ -15,27 +15,5 @@ namespace TelegramBotFramework.ConsoleApp
         }
 
     }
-    public class NewTeleBot : TelegramBotWrapper
-    {
-        public int sd;
-        public NewTeleBot(int val) : base("855824339:AAH8b31siVTDZG_Eu7RMQDz4IF0GPYzK4O0", 166938818, null, "MarketMaking")
-        {
-            sd = val;
-        }
-    }
-    [TelegramBotModule(Author = "ridicoulous", Name = "MarketMaking", Version = "1.0")]
-    public class MarketMakingBotModule : TelegramBotModuleBase
-    {
-        NewTeleBot _wrapper;
-        public MarketMakingBotModule(NewTeleBot wrapper) : base(wrapper)
-        {
-            _wrapper = wrapper;
-        }
-        [ChatCommand(Triggers = new[] { "b" }, HideFromInline = true, DontSearchInline = true)]
-        public CommandResponse GetConfigs(CommandEventArgs args)
-        {
-            var balance = int.Parse(args.Parameters)*_wrapper.sd;
-            return new CommandResponse($"Your balance is *{balance}*", parseMode: ParseMode.Markdown);
-        }
-    }
+    
 }
